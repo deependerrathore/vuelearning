3269db69794a268dd4777e80f3aee7c06cafa28c
Deepender Rathore <deepenrathore28@gmail.com>
2019-03-05 12:19:30 +1100

    router-link

--- support-center/src/components/NavMenu.vue
+++ support-center/src/components/NavMenu.vue
@@ -1,11 +1,10 @@
 <template>
-    <v-toolbar>
+    <v-toolbar absolute color="teal lighten-3">
         <v-toolbar-title caps>MY SHOPPING CART</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-            <v-btn flat>Link One</v-btn>
-            <v-btn flat>Link Two</v-btn>
-            <v-btn flat>Link Three</v-btn>
+            <router-link :to="{name: 'home' }">Home</router-link>
+            <router-link :to="{name: 'faq' }">FAQ</router-link>
         </v-toolbar-items>
     </v-toolbar>
 </template>
