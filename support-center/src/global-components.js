34f0cfed81c4ee69856ac70e0ac99edc23b17281
Deepender Rathore <deepenrathore28@gmail.com>
2019-03-18 14:44:04 +1100

    Hello World

--- support-center/src/global-components.js
+++ support-center/src/global-components.js
@@ -1,4 +1,6 @@
 import Vue from 'vue';
+import SmartForm from './components/SmartForm.vue';
 import FormInput from './components/FormInput.vue';
 
 Vue.component('FormInput',FormInput);
+Vue.component('SmartForm',SmartForm);
